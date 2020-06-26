0177b442447d136a07f20b4f13e6d9801fc24c1f
jcrf15 <59666296+jcrf15@users.noreply.github.com>
2020-06-26 14:41:14 -0700

    submenu fieldprograms

--- js/submenuaction.js
+++ js/submenuaction.js
@@ -1,4 +1,8 @@
-const url_string = window.location;
+// recover URL
+const url_string = window.location;  
+// we create a object URL
 let url = new URL(url_string);
+// look for the parameter ("par")
 let option = url.searchParams.get("par");
+// simulate the click with the ID
 document.getElementById(option).click();
